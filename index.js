4be79cc611b4025d8535db49b8d8054d61c27c39
AntoPraveen004 <131511795+AntoPraveen004@users.noreply.github.com>
2023-04-22 18:02:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function findDifference(a, b) {
+  // This function takes in two parameters a and b,
+  // and returns the difference between them.
+  var difference = a - b;
+  return difference;
+}
+var num1 = 10;
+var num2 = 5;
+var result = findDifference(num1, num2);
+console.log("The difference is: " + result);
